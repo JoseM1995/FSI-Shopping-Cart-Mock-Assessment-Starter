ecd1dab3013d67c822e26bc9a0b7b73bf2847f2a
Jose Montoya <josem@Joses-MacBook-Pro.local>
2022-06-28 20:57:35 -0700

    done

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
+// update quantity
 function updateQuantity(displayQuantity) {
     let quantity = document.querySelector('.total-quantity')
     quantity.innerHTML = displayQuantity 
 }
 
-// product already in cart so quant = 1 
+// Start quantity at 1 since product is in cart
 let quantity = 1
 
-// buttons
-
+// Buttons
 const quantDown = document.querySelector('#quantity-down')
 const quantUp = document.querySelector('#quantity-up')
 
 // add event listener to decrease quantity
-quantDown.addEventListener('click', function(e)){
+quantDown.addEventListener('click', function(e){
     if (quantity > 0) {
         quantity--
     }
